b2cff1bbfc3c14c0b0d5761a66780a16cc2b1792
Kyle Zunino <kzunino@gmail.com>
2019-03-09 12:01:44 -0800

    added comments.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,6 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
 
 //Here I created an array containing 6 objects that function as the quotes for my project.
+//properties include: quote, source, citation (if available), year (if available).
 
 var quotes = [
   {quote: '¡Qué vergüenza!', source: 'Ana Salamanca', citation: 'Bendito Seas, Colombia'},
@@ -29,12 +14,6 @@ var quotes = [
   {quote: 'When the student is ready, the teacher appears', source: 'Dave', year: 2016},
 ]
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
 /*'getRandomQuote' function gets a random quote object from the array.
   Since array contains 6 objects starting from index value 0 to 5 I set the upper
   limit of the math function to 5 and the lower limit to 0.
@@ -44,36 +23,25 @@ function getRandomQuote(randomQuote) {
     var randomQuote = quotes[Math.floor(Math.random() * (5 - 0 + 1)) + 0];
     return randomQuote;
 }
-/***
-  Create the `printQuote` function to:
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
+//-printQuote function sets the random quote to call the getRandomQuote function and stores as a variable.
 
-function printQuote() {
+function printQuote(html) {
     var randomQuote = getRandomQuote();
     var html = ' ';
-    html += '<p class="quote">' + randomQuote.quote + '</p>';
+    html += '<p class="quote">' + randomQuote.quote + '</p>';  //-html combines first two properties of objects
     html += '<p class="source">' + randomQuote.source + '';
-if (randomQuote.citation!== undefined) {
+if (randomQuote.citation!== undefined) {                        //-undefined is use in place of null because if property is not found null prints "undefined to string"
     html += '<span class="citation">' + randomQuote.citation + '</span>';
-  }else if (randomQuote.citation === undefined) {
-    ;
+      }else if (randomQuote.citation === undefined) {
+        ;                                               //-else if statement has a ';' to allow fucntion to take no action--i.e. no html or string is added to html variable. Source: MDN
   }
- if (randomQuote.year !== undefined) {
+if (randomQuote.year !== undefined) {
     html += '<span class="year">' + randomQuote.year + '</span>';
-  }else if (randomQuote.year === undefined){
-    ;
+      }else if (randomQuote.year === undefined){
+        ;
   }
-  html += '</p>';
-  return html;
+    html += '</p>';
+    return html;      //-return html returns strings to html after conditions have been tested on the object.
   }
 document.getElementById('quote-box').innerHTML = printQuote();
 
@@ -84,7 +52,4 @@ document.getElementById('quote-box').innerHTML = printQuote();
   comment.
 ***/
 
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+document.getElementById("loadQuote").addEventListener("click", printQuote, false);
